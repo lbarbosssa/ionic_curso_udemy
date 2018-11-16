5e355f8fc28dc97078da6fe614a9ae4ed8119bcb
Lucas Barbosa <barbosa.lucas1996@gmail.com>
2018-11-16 14:11:52 -0200

    onDelete

--- ionicSQLite/src/models/movie.model.ts
+++ ionicSQLite/src/models/movie.model.ts
@@ -1,6 +1,6 @@
 export class Movie {
    
-    public id: string;
+    public id: number;
     //O id não está no contruct pois ele é gerado pelo SQlite
     
     constructor(
--- ionicSQLite/src/pages/home/home.ts
+++ ionicSQLite/src/pages/home/home.ts
@@ -20,71 +20,95 @@ export class HomePage {
     public loadingCtrl: LoadingController,
     public movieService: MovieService,
     public navCtrl: NavController
-    ) {}
+  ) { }
 
-    onSave(type: string, item?: ItemSliding, movie?: Movie ): void{
-      let title: string = type.charAt(0).toUpperCase() + type.substr(1);
-      this.showAlert({
-        itemSliding: item,
-        title: `${title} movie`,
-        type: type,
-        movie: movie
-      });
+  onSave(type: string, item?: ItemSliding, movie?: Movie): void {
+    let title: string = type.charAt(0).toUpperCase() + type.substr(1);
+    this.showAlert({
+      itemSliding: item,
+      title: `${title} movie`,
+      type: type,
+      movie: movie
+    });
+  }
 
-    }
+  onDelete(movie: Movie): void{
+    this.alertCtrl.create({
+      title: `Do you want to delete ${movie.title} movie ?`,
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
 
-    private showAlert(options: {itemSliding?: ItemSliding, title: string, type: string, movie?: Movie}): void{
-      let alertOptions: AlertOptions = {
-        title: options.title,
-        inputs: [
-          {
-            name: 'title',
-            placeholder: 'Movie title'
+            let Loading: Loading = this.showLoading(`Deleting '${movie.title}' ...`);
+            this.movieService.delete(movie.id)
+              .then((deleted: boolean) => {
+                if (deleted){
+                  this.movies.splice(this.movies.indexOf(movie), 1);
+                }
+                Loading.dismiss();
+              })
+
           }
-        ],
-        buttons: [
-          'Calcel',
-          {
-            text: 'Save',
-            handler: (data) => {
+        },
+        'No'
+      ]
+    }).present();
+  }
 
-              let Loading: Loading = this.showLoading(`Saving ${data.title} movie...`);
-              let contextMovie: Movie;
-              
-              switch(options.type){
-                case 'crete':
-                  contextMovie = new Movie(data.title);
-                  break;
-                
-                case 'update':
-                  options.movie.title = data.title;
-                  contextMovie = options.movie;
-                  break;
-              }
 
-              this.movieService[options.type](contextMovie)
-                .then((result: any) => {
-                  if(options.type === 'create') this.movies.unshift(result);
-                  Loading.dismiss();
-                  if (options.itemSliding) options.itemSliding.close();
-                });
+  private showAlert(options: { itemSliding?: ItemSliding, title: string, type: string, movie?: Movie }): void {
+    let alertOptions: AlertOptions = {
+      title: options.title,
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Movie title'
+        }
+      ],
+      buttons: [
+        'Calcel',
+        {
+          text: 'Save',
+          handler: (data) => {
+
+            let Loading: Loading = this.showLoading(`Saving ${data.title} movie...`);
+            let contextMovie: Movie;
+
+            switch (options.type) {
+              case 'crete':
+                contextMovie = new Movie(data.title);
+                break;
+
+              case 'update':
+                options.movie.title = data.title;
+                contextMovie = options.movie;
+                break;
             }
+
+            this.movieService[options.type](contextMovie)
+              .then((result: any) => {
+                if (options.type === 'create') this.movies.unshift(result);
+                Loading.dismiss();
+                if (options.itemSliding) options.itemSliding.close();
+              });
           }
-        ]
-      };
-      if (options.type === 'update'){
-        alertOptions.inputs[0]['value'] = options.movie.title;
-      }
-      this.alertCtrl.create(alertOptions).present();
+        }
+      ]
+    };
+    if (options.type === 'update') {
+      alertOptions.inputs[0]['value'] = options.movie.title;
     }
+    this.alertCtrl.create(alertOptions).present();
+  }
 
-    private showLoading(message?: string): Loading{
-      let Loading: Loading = this.loadingCtrl.create({
-        content: message || 'Please wait ...'
-      });
-        Loading.present();
-        return Loading;
+  private showLoading(message?: string): Loading {
+    let Loading: Loading = this.loadingCtrl.create({
+      content: message || 'Please wait ...'
+    });
+    Loading.present();
+    return Loading;
 
-    }
+  }
 
 }

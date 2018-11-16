88ab5363815ff60f84681953d77b18b7473fc1e7
Lucas Barbosa <barbosa.lucas1996@gmail.com>
2018-11-16 13:24:43 -0200

    showAler e showLoading

--- ionicSQLite/src/pages/home/home.ts
+++ ionicSQLite/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ItemSliding, AlertOptions, Loading, LoadingController } from 'ionic-angular';
 import { Movie } from '../../models/movie.model';
+import { MovieService } from '../../providers/movie/movie.service';
 
 @Component({
   selector: 'page-home',
@@ -14,8 +15,61 @@ export class HomePage {
 
   ];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    public movieService: MovieService,
+    public navCtrl: NavController
+    ) {}
 
-  }
+    private showAlert(options: {itemSliding?: ItemSliding, title: string, type: string, movie?: Movie}): void{
+      let alertOptions: AlertOptions = {
+        title: options.title,
+        inputs: [
+          {
+            name: 'title',
+            placeholder: 'Movie title'
+          }
+        ],
+        buttons: [
+          'Calcel',
+          {
+            text: 'Save',
+            handler: (data) => {
+
+              let Loading: Loading = this.showLoading(`Saving ${data.title} movie...`);
+              let contextMovie: Movie;
+              
+              switch(options.type){
+                case 'crete':
+                  contextMovie = new Movie(data.title);
+                  break;
+                
+                case 'update':
+                  options.movie.title = data.title;
+                  contextMovie = options.movie;
+                  break;
+              }
+
+              this.movieService[options.type](contextMovie)
+                .then((result: any) => {
+                  if(options.type === 'create') this.movies.unshift(result);
+                  Loading.dismiss();
+                  if (options.itemSliding) options.itemSliding.close();
+                });
+            }
+          }
+        ]
+      }
+    }
+
+    private showLoading(message?: string): Loading{
+      let Loading: Loading = this.loadingCtrl.create({
+        content: message || 'Please wait ...'
+      });
+        Loading.present();
+        return Loading;
+
+    }
 
 }
